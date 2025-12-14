e239074aea195018f522eae3c898a3c068e1c61e
Sayan-Majumdar-06 <sayanmajumdar.edu.3101@gmail.com>
2025-12-14 10:20:46 +0530

    Added solution to 1 medium array problem from Striver's A2Z DSA Sheet

---
+++ dsa_practice/Striver's A2Z DSA/Arrays/LC_Rotate_image_medium.cpp
@@ -0,0 +1,28 @@
+// Leetcode Problem : Rotate Image
+// Difficulty : Medium
+// Link : https://leetcode.com/problems/rotate-image/
+// Approach : Optimal
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
